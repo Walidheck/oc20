5adc650d2233edb3022b4a336167b9fa1e1a5f83
Walidheck <walid.heckendorn@eduvaud.ch>
2021-02-09 09:00:54 +0100

    image_example
    bouger avec souris

--- TUTORIAL/2_rectangles/rect1.py
+++ TUTORIAL/2_rectangles/rect1.py
@@ -8,13 +8,13 @@ GRAY = (150, 150, 150)
 pygame.init()
 screen = pygame.display.set_mode(SIZE)
 
-rect = Rect(50, 100, 60, 80)
-r2 = Rect(200, 200, 100, 100)
+img = pygame.image.load('bird.png')
+img.convert()
+
+
+rect = img.get_rect()
 print(rect)
 
-print(f'x={rect.x}, y={rect.y}, w={rect.w}, h={rect.h}')
-print(f'left={rect.left}, top={rect.top}, right={rect.right}, bottom={rect.bottom}')
-print(f'center={rect.center}')
 
 running = True
 while running:
